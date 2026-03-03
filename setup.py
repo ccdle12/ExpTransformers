10f3af9d1c94a7c428c5d941a8d718dd9c324809
ccdle12 <d24707u8@4wrd.cc>
2026-03-03 12:09:13 +0000

    add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ExpTransformers',
+    version='0.1.0',
+    author='ccdle12',
+    author_email='x2qawobb@anonaddy.me',
+    description='An experimental transformers library',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+)
+
